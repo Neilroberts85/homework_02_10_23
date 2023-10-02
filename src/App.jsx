cbda300b173464e0a2f681b0eec3c10e243179e4
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-10-02 19:43:01 +0100

    finished extensions

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,38 @@ import './App.css'
 function App() {
   
   const [items, setItems] = useState([
-    {name: "Buy shopping", priority: "High"},
-    {name: "Clean bathroom", priority: "High"}, 
+    {name: "Buy shopping", priority: "high"},
+    {name: "Clean bathroom", priority: "high"}, 
     {name: "Car's MOT", priority: "low"}
   ])
 
   const [newItem, setNewItem] = useState("")
+  const [newPriority, setNewPriority] = useState("")
   
 
   const itemList = items.map((item, index) => {
-    return <li key={index} className={item.priority ? "high":"low"}>{item.name}{item.priority}</li>
+    return <li key={index} className={item.priority ? "high":"low"}>
+      <span className='item-name'>{item.name}</span>
+      {item.priority === "high" ? (
+        <span className='high'>High Priority!</span>
+      ) : (
+        <span className='low'>Low Priority!</span>)}
+      </li>
   })
   
   const handleItemInput = (event) => {
     setNewItem(event.target.value)
   }
 
+  const handleRadioInput = (event) => {
+    setNewPriority(event.target.value)
+  }
+
   const saveNewItem = (event) => {
     event.preventDefault()
     const copyItems = [... items]
     
-    copyItems.push({name: newItem, priority: "low"})
+    copyItems.push({name: newItem, priority: newPriority})
     setItems(copyItems)
     setNewItem("")
 
@@ -40,12 +51,12 @@ console.log(itemList)
      <ul>
       {itemList}
      </ul>
-    <form on onSubmit={saveNewItem}>
+    <form onSubmit={saveNewItem}>
       <label htmlFor='new-todo'>Add a To do</label>
       <input id="new-todo" type='text' value={newItem} onChange={handleItemInput}/>
-      <input id="radio" name="priority" type='radio'value={newItem} onChange={handleItemInput}/>
+      <input id="radio-high" name="priority" type='radio'value="high" onChange={handleRadioInput}/>
       <label htmlFor="high-priority">High Priority</label>
-      <input id="radio" name="priority" type='radio' value={newItem} onChange={handleItemInput}/>
+      <input id="radio-low" name="priority" type='radio' value="low" onChange={handleRadioInput}/>
       <label htmlFor="low-priority">Low Priority</label>
       <input type='submit' value="Save new Todo"/> 
 
